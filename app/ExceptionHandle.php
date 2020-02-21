394d211e59470f01c0c55a791fee03f73def62ec
gjianbo <gjianbo@qq.com>
2020-02-21 21:41:32 +0800

    改成了异常类的处理

--- app/ExceptionHandle.php
+++ app/ExceptionHandle.php
@@ -2,6 +2,7 @@
 namespace app;
 
 use think\db\exception\DataNotFoundException;
+use think\db\exception\DbException;
 use think\db\exception\ModelNotFoundException;
 use think\exception\Handle;
 use think\exception\HttpException;
@@ -37,11 +38,24 @@ class ExceptionHandle extends Handle
      */
     public function report(Throwable $exception): void
     {
-        // 使用内置的方式记录异常日志
 
-        if(!($exception instanceof  HttpResponseException)){
+        // 处理数据库的异常
+        if ($exception instanceof DbException) {
             Logger::exception($exception);
+            api_result(API_ERROR, $exception->getMessage());
+
+        } elseif (!($exception instanceof HttpResponseException)) {
+            //不记录404的异常信息
+            if (method_exists($exception, 'getStatusCode') && $exception->getStatusCode() == 404) {
+                $msg = $exception->getMessage();
+                //$msg = substr($msg, 0, strpos($msg, ":") + 1) . substr($msg, strrpos($msg, '\\') + 1);
+                api_result(API_ERROR, $msg);
+            } else {
+                Logger::exception($exception);
+            }
+
         }
+        // 使用内置的方式记录异常日志
         parent::report($exception);
     }
 
